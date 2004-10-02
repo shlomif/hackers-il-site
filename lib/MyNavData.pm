d5e7639b5d2592ebd0460a61f2ec07c59e60813f
shlomif <shlomif@124812d1-1e0d-29c7-c697-926a49d246de>
2004-10-02 18:07:34 +0000

    Hackers-IL Site: importing the files into the repository from my local copy

---
+++ lib/MyNavData.pm
@@ -0,0 +1,43 @@
+package MyNavData;
+
+my $hosts =
+{
+    'hackers' => 
+    {
+        'base_url' => "http://www.hackers.org.il/",
+    },
+};
+
+my $tree_contents =
+{
+    'host' => "hackers",
+    'value' => "The Hackers-IL Homepage",
+    'title' => "Hackers-IL: Israeli Software Enthusiasts",
+    'expand_re' => "",
+    'subs' =>
+    [
+        {
+            'value' => "Home",
+            'url' => "",
+        },
+        {
+            'value' => "Wiki",
+            'url' => "wiki/",
+        },
+        {
+            'value' => "Mailing List",
+            'url' => "mailing-list/",
+        },
+    ],
+};
+
+sub get_params
+{
+    return 
+        (
+            'hosts' => $hosts,
+            'tree_contents' => $tree_contents,
+        );
+}
+
+1;
